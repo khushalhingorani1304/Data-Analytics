9ae11ff00f1974d5bc593c5c84f34dcf7871210c
khushalhingorani1304 <khushalhingorani1304@gmail.com>
2024-02-24 21:48:35 +0530

    1

---
+++ list.py
@@ -0,0 +1,39 @@
+x = [int(x) for x in input().split()]
+
+# cnt = 0
+# i1 = 0
+# i = 0
+# i2=0
+
+# while(i<len(x)-1):
+#     if(x[i]<x[i+1]):
+#         cnt=cnt+1
+#         i=i+1
+#         i2 = cnt
+
+#     else:
+#         if(i1<=i2):
+#             l = i1
+#             i1=i2
+#         cnt = 0
+#         i=i+1
+
+# if(i1<=i2):
+#     l = i1
+#     i1 = i2
+
+
+# if(l==0):
+#     print(x[:l+i1+1])
+# else:
+#     print(x[l+cnt-i2+1:l+i1+1+cnt])
+# i = 0
+# k = 0
+# while(i<len(x)-1):
+#     while(x[i]<x[i+1]):
+#         l1 = []
+#         l1.append(x[i])
+#         i=i+1
+
+
+
